f617d5fe6481ba8a0294893f973edbc58c5b2bd6
steven wong <wong.steven604@gmail.com>
2021-12-13 12:45:27 -0800

    add ui router and skeleton components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Profile from './pages/Profile'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/:username" element={<Profile/>}  />
+        <Route exact path="/" element={<Home/>}  />
+      </Routes>
+    </Router>
   );
 }
 
