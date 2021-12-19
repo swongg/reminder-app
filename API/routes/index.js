4827545c832e1c8c8bf8465287c88d4aeb58aee1
steven wong <wong.steven604@gmail.com>
2021-12-18 16:29:18 -0800

    update gitignore files

--- API/routes/index.js
+++ API/routes/index.js
@@ -8,7 +8,11 @@ router.get("/", (req, res, next) => {
   res.send("HELLO WORLD");
 });
 
+// router.use(`${process.env.BASE_API_URL}/auth`, auth);
+// router.use(`${process.env.BASE_API_URL}/reminder`, reminder);
 router.use("/auth", auth);
 router.use("/reminder", reminder);
+
+
 
 module.exports = router;
