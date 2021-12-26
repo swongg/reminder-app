4089d51402ca7c8ec0a0013098ffb04f4e9e868c
steven wong <wong.steven604@gmail.com>
2021-12-26 11:30:52 -0800

    update registration route

--- API/routes/auth.js
+++ API/routes/auth.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const { to } = require("await-to-js");
+const { verifyPassword, hashPassword } = require("../auth/utils");
+const { login } = require("../auth/strategies/jwt");
+const { createUser, getUserByEmail } = require("../controllers/user");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  res.send("AUTH");
-});
+// router.get("/", (req, res, next) => {
+//   res.send("AUTH");
+// });
 
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
@@ -12,10 +16,47 @@ router.post("/login", async (req, res) => {
   return res.status(200).json({ success: true, data });
 });
 
-router.post("/register", async (req, res) => {
-  const { firstName, lastName, email, password } = req.body;
+router.post('/register', async (req, res) => {
+  const { firstName, lastName, email, password } = req.body
 
-  return res.status(200).json({ success: true, data: null });
-});
+  if (!/\b\w+\@\w+\.\w+(?:\.\w+)?\b/.test(email)) {
+    return res.status(500).json({ success: false, data: 'Enter a valid email address.' })
+  } else if (password.length < 5 || password.length > 20) {
+    return res.status(500).json({
+      success: false,
+      data: 'Password must be between 5 and 20 characters.'
+    })
+  }
+
+  let [err, user] = await to(
+    createUser({
+      firstName,
+      lastName,
+      email,
+      password: await hashPassword(password)
+    })
+  )
+
+  if (err) {
+    return res.status(500).json({ success: false, data: 'Email is already taken' })
+  }
+
+  const [loginErr, token] = await to(login(req, user))
+
+  if (loginErr) {
+    console.error(loginErr)
+    return res.status(500).json({ success: false, data: 'Authentication error!' })
+  }
+
+  return res
+    .status(200)
+    .cookie('jwt', token, {
+      httpOnly: true
+    })
+    .json({
+      success: true,
+      data: '/'
+    })
+})
 
 module.exports = router;
